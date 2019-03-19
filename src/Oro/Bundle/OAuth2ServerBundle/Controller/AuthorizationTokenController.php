322f73162a1b2162f9f5c7382385bd0af4d47df9
Vova Soroka <vsoroka@oroinc.com>
2019-03-18 23:01:23 -0500

    BAP-16477: OAuth 2.0 Client Credential Flow for Management API (#23048)
    - add "copy to clipboard" button for "Client ID" field on "Create OAuth Application" form
    - add "Client ID" read-only filed to "Edit OAuth Application" form
    - enable "OPTION" method for "/oauth2-token" endpoint
    - add CORS support for "/oauth2-token" endpoint

--- src/Oro/Bundle/OAuth2ServerBundle/Controller/AuthorizationTokenController.php
+++ src/Oro/Bundle/OAuth2ServerBundle/Controller/AuthorizationTokenController.php
@@ -9,6 +9,8 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 use Zend\Diactoros\Response;
 
 /**
@@ -35,6 +37,56 @@ class AuthorizationTokenController extends Controller
 
             return $e->generateHttpResponse($serverResponse);
         }
+    }
+
+    /**
+     * Handles OPTIONS HTTP method for OAuth 2.0 access token endpoint.
+     *
+     * @param SymfonyRequest $request
+     *
+     * @return SymfonyResponse
+     */
+    public function optionsAction(SymfonyRequest $request): SymfonyResponse
+    {
+        $response = new SymfonyResponse();
+        $response->headers->set('Allow', 'OPTIONS, POST');
+        if ($this->isCorsRequest($request)) {
+            $origin = $request->headers->get('Origin');
+            $allowedOrigins = $this->getParameter('oro_oauth2_server.cors.allow_origins');
+            if (in_array($origin, $allowedOrigins, true)) {
+                $response->headers->set('Access-Control-Allow-Origin', $origin);
+            }
+            $requestMethod = $request->headers->get('Access-Control-Request-Method');
+            if ($requestMethod) {
+                $response->headers->set('Access-Control-Allow-Methods', $response->headers->get('Allow'));
+                $response->headers->remove('Allow');
+                $response->headers->set('Access-Control-Allow-Headers', 'Content-Type');
+                $preflightMaxAge = $this->getParameter('oro_oauth2_server.cors.preflight_max_age');
+                if ($preflightMaxAge > 0) {
+                    $response->headers->set('Access-Control-Max-Age', $preflightMaxAge);
+                    // although OPTIONS requests are not cacheable, add "Cache-Control" header
+                    // indicates that a caching is enabled to prevent making CORS preflight requests not cacheable
+                    $response->headers->set('Cache-Control', sprintf('max-age=%d, public', $preflightMaxAge));
+                    // the response depends on the Origin header value and should therefore not be served
+                    // from cache for any other origin
+                    $response->headers->set('Vary', 'Origin');
+                }
+            }
+        }
+
+        return $response;
+    }
+
+    /**
+     * @param SymfonyRequest $request
+     *
+     * @return bool
+     */
+    private function isCorsRequest(SymfonyRequest $request): bool
+    {
+        return
+            $request->headers->has('Origin')
+            && $request->headers->get('Origin') !== $request->getSchemeAndHttpHost();
     }
 
     /**
