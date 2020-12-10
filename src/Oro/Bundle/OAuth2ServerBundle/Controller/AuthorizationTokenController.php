549854de715c30404f8c2355f05b9a1b86106015
Vova Soroka <vsoroka@oroinc.com>
2020-12-10 11:37:36 +0200

    BAP-20209: "oauth-token" post request does not support CORS

--- src/Oro/Bundle/OAuth2ServerBundle/Controller/AuthorizationTokenController.php
+++ src/Oro/Bundle/OAuth2ServerBundle/Controller/AuthorizationTokenController.php
@@ -33,13 +33,19 @@ class AuthorizationTokenController extends Controller
             $response = $this->getAuthorizationServer()
                 ->respondToAccessTokenRequest($serverRequest, $serverResponse);
             $this->get('oro_oauth2_server.handler.get_access_token.success_handler')->handle($serverRequest);
-
-            return $response;
         } catch (OAuthServerException $e) {
             $this->get('oro_oauth2_server.handler.get_access_token.exception_handler')->handle($serverRequest, $e);
-
-            return $e->generateHttpResponse($serverResponse);
+            $response = $e->generateHttpResponse($serverResponse);
         }
+
+        if ($this->isCorsTokenRequest($serverRequest)) {
+            $origin = $this->getTokenRequestOrigin($serverRequest);
+            if ($this->isAllowedOrigin($origin)) {
+                $response = $response->withHeader('Access-Control-Allow-Origin', $origin);
+            }
+        }
+
+        return $response;
     }
 
     /**
@@ -55,8 +61,7 @@ class AuthorizationTokenController extends Controller
         $response->headers->set('Allow', 'OPTIONS, POST');
         if ($this->isCorsRequest($request)) {
             $origin = $request->headers->get('Origin');
-            $allowedOrigins = $this->getParameter('oro_oauth2_server.cors.allow_origins');
-            if (in_array($origin, $allowedOrigins, true)) {
+            if ($this->isAllowedOrigin($origin)) {
                 $response->headers->set('Access-Control-Allow-Origin', $origin);
             }
             $requestMethod = $request->headers->get('Access-Control-Request-Method');
@@ -90,6 +95,57 @@ class AuthorizationTokenController extends Controller
         return
             $request->headers->has('Origin')
             && $request->headers->get('Origin') !== $request->getSchemeAndHttpHost();
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     *
+     * @return bool
+     */
+    private function isCorsTokenRequest(ServerRequestInterface $request): bool
+    {
+        return
+            $request->hasHeader('Origin')
+            && $this->getTokenRequestOrigin($request) !== $this->getTokenRequestSchemeAndHttpHost($request);
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     *
+     * @return string
+     */
+    private function getTokenRequestOrigin(ServerRequestInterface $request): string
+    {
+        $value = $request->getHeader('Origin');
+
+        return reset($value);
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     *
+     * @return string
+     */
+    private function getTokenRequestSchemeAndHttpHost(ServerRequestInterface $request): string
+    {
+        $uri = $request->getUri();
+        $result = $uri->getScheme() . '://' . $uri->getHost();
+        $port = $uri->getPort();
+        if (null !== $port) {
+            $result .= ':' . (string)$port;
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param string $origin
+     *
+     * @return bool
+     */
+    private function isAllowedOrigin(string $origin): bool
+    {
+        return \in_array($origin, $this->getParameter('oro_oauth2_server.cors.allow_origins'), true);
     }
 
     /**
