8a04b3a7028fe0e2a34eeec8a4c7636be5fe56ad
Alex Yegerev <1755578+lexxcode@users.noreply.github.com>
2025-10-15 18:01:11 +0400

    BB-26263: Convert for marketing, microsoft-sync, oauth2-server, stripe, task, test-security (#42718)

--- src/Oro/Bundle/OAuth2ServerBundle/Resources/public/js/views/client-grant-view.js
+++ src/Oro/Bundle/OAuth2ServerBundle/Resources/public/js/views/client-grant-view.js
@@ -1,48 +1,44 @@
-define(function(require) {
-    'use strict';
+import BaseView from 'oroui/js/app/views/base/view';
+import $ from 'jquery';
 
-    const BaseView = require('oroui/js/app/views/base/view');
-    const $ = require('jquery');
+const GrantWatcherView = BaseView.extend({
+    events: {
+        click: 'updateClientView'
+    },
 
-    const GrantWatcherView = BaseView.extend({
-        events: {
-            click: 'updateClientView'
-        },
+    /**
+     * @inheritdoc
+     */
+    constructor: function GrantWatcherView(options) {
+        GrantWatcherView.__super__.constructor.call(this, options);
+    },
 
-        /**
-         * @inheritdoc
-         */
-        constructor: function GrantWatcherView(options) {
-            GrantWatcherView.__super__.constructor.call(this, options);
-        },
+    initialize: function(options) {
+        this.ownerField = $(options.ownerField);
+        this.redirectUrisField = $(options.redirectUrisField);
+        this.confidentialField = $(options.confidentialField);
+        this.skipAuthorizeClientAllowedField = $(options.skipAuthorizeClientAllowedField);
 
-        initialize: function(options) {
-            this.ownerField = $(options.ownerField);
-            this.redirectUrisField = $(options.redirectUrisField);
-            this.confidentialField = $(options.confidentialField);
-            this.skipAuthorizeClientAllowedField = $(options.skipAuthorizeClientAllowedField);
+        this.updateClientView();
+    },
 
-            this.updateClientView();
-        },
-
-        updateClientView: function() {
-            const selectedGrant = this.$el.filter(':checked').val();
-            if ('client_credentials' === selectedGrant) {
-                this.ownerField.removeClass('hide');
-            } else {
-                this.ownerField.addClass('hide');
-            }
-            if ('authorization_code' === selectedGrant) {
-                this.redirectUrisField.removeClass('hide');
-                this.confidentialField.removeClass('hide');
-                this.skipAuthorizeClientAllowedField.removeClass('hide');
-            } else {
-                this.redirectUrisField.addClass('hide');
-                this.confidentialField.addClass('hide');
-                this.skipAuthorizeClientAllowedField.addClass('hide');
-            }
+    updateClientView: function() {
+        const selectedGrant = this.$el.filter(':checked').val();
+        if ('client_credentials' === selectedGrant) {
+            this.ownerField.removeClass('hide');
+        } else {
+            this.ownerField.addClass('hide');
         }
-    });
-
-    return GrantWatcherView;
+        if ('authorization_code' === selectedGrant) {
+            this.redirectUrisField.removeClass('hide');
+            this.confidentialField.removeClass('hide');
+            this.skipAuthorizeClientAllowedField.removeClass('hide');
+        } else {
+            this.redirectUrisField.addClass('hide');
+            this.confidentialField.addClass('hide');
+            this.skipAuthorizeClientAllowedField.addClass('hide');
+        }
+    }
 });
+
+export default GrantWatcherView;
