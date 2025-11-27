b0d462a70f42e3bf5d75062f81cd9c3708e6a36d
Alex Yegerev <1755578+lexxcode@users.noreply.github.com>
2025-11-27 13:16:59 +0400

    BB-26283: Convert for UIBundle (#42744)
    * BB-26268: Convert for TranslationBundle, ThemeBundle, TestFrameworkBundle, TagBundle, SyncBundle
    
    * BB-26277: Convert for LocaleBundle
    
    * BB-26283: Convert for UIBundle

--- src/Oro/Bundle/OAuth2ServerBundle/Resources/public/js/views/api-types-view.js
+++ src/Oro/Bundle/OAuth2ServerBundle/Resources/public/js/views/api-types-view.js
@@ -1,35 +1,31 @@
-define(function(require) {
-    'use strict';
+import BaseView from 'oroui/js/app/views/base/view';
+import $ from 'jquery';
 
-    const BaseView = require('oroui/js/app/views/base/view');
-    const $ = require('jquery');
+const ApiTypesView = BaseView.extend({
+    events: {
+        click: 'updateApiTypesView'
+    },
 
-    const ApiTypesView = BaseView.extend({
-        events: {
-            click: 'updateApiTypesView'
-        },
+    /**
+     * @inheritdoc
+     */
+    constructor: function ApiTypesView(options) {
+        ApiTypesView.__super__.constructor.call(this, options);
+    },
 
-        /**
-         * @inheritdoc
-         */
-        constructor: function ApiTypesView(options) {
-            ApiTypesView.__super__.constructor.call(this, options);
-        },
+    initialize: function(options) {
+        this.apiTypesField = $(options.apiTypesField);
 
-        initialize: function(options) {
-            this.apiTypesField = $(options.apiTypesField);
+        this.updateApiTypesView();
+    },
 
-            this.updateApiTypesView();
-        },
-
-        updateApiTypesView: function() {
-            if (this.$el.find('input').is(':checked')) {
-                this.apiTypesField.addClass('hide');
-            } else {
-                this.apiTypesField.removeClass('hide');
-            }
+    updateApiTypesView: function() {
+        if (this.$el.find('input').is(':checked')) {
+            this.apiTypesField.addClass('hide');
+        } else {
+            this.apiTypesField.removeClass('hide');
         }
-    });
-
-    return ApiTypesView;
+    }
 });
+
+export default ApiTypesView;
